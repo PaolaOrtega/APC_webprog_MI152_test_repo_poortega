e40154fe06f4fa159d2b60bed501faeb9c70c37c
PaolaOrtega <pfortega@outlook.com>
2016-10-13 09:59:50 +0800

    Create demo3.php

---
+++ PHP-files/demo3.php
@@ -0,0 +1,11 @@
+<?php
+function familyName($fname) {
+    echo "$fname Ortega.<br>";
+}
+
+familyName("Paola");
+familyName("Patricia");
+familyName("Giuseppe");
+familyName("Joseph");
+familyName("Ian");
+?>
