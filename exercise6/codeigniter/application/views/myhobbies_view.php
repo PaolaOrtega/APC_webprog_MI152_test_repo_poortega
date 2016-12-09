756f0a75ae82c13dc9eb80ef9161f99683b11107
PaolaOrtega <pfortega@outlook.com>
2016-12-09 19:30:00 +0800

    Created myhobbies_view.php

---
+++ exercise6/codeigniter/application/views/myhobbies_view.php
@@ -0,0 +1,130 @@
+<!DOCTYPE HTML>
+<html>
+<title>
+	Paola Ortega - myHobbies
+</title>
+	
+<head>
+	
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+	
+	<style>
+html {
+  	background: url(piano.jpg) no-repeat center center fixed; 
+  	-webkit-background-size: cover;
+  	-moz-background-size: cover;
+  	-o-background-size: cover;
+  	background-size: cover;
+}
+
+.social-icons {
+	font-size: 20px;
+	text-align: Center;
+	position: relative;
+	top: 127px;
+}
+
+.social-icons a {
+	color: white;
+}
+
+body{
+	font-family: Century Gothic, sans-serif;
+}
+
+*{
+	padding: 0px;
+	margin: 0px;
+}
+
+#navdiv{
+	opacity: 0.7;
+	filter: (opacity=70);
+}
+
+#navdiv ul{
+	width: 100%;
+	height: 80px;
+	background: #1f1f1f;
+	line-height: 80px;
+	color: white;
+	margin-right: 15px;
+}
+
+#navdiv ul a{
+	text-decoration: none;
+	color: white;
+	padding: 20px;
+}
+
+#navdiv ul li{
+	list-style-type: none;
+	display: inline-block;
+	float: right;
+}
+
+#navdiv ul li:hover{
+	background: #8b8683;
+	transition:all 0.40s;
+}
+
+#navdiv h1{
+	width: 300px;
+	float: left;
+	cursor: pointer;
+	margin-left: 15px;
+}
+
+.prof2-box{
+	width: 665px;
+	height: 320px;
+	background: rgba(0,0,0,0.4);
+	padding: 40px;
+	color: white;
+	margin: 0 auto;
+	margin-top: 60px;
+	text-align: center;
+}
+
+.hobby{
+	letter-spacing: 10px;
+	margin-bottom: 20px;
+	font-weight: bold;
+}
+
+.par1{
+	margin-top: 30px;
+}
+	</style>
+</head>
+<body>
+<div id="Maindiv">
+	<div id="navdiv">
+		
+		<ul>
+			<h1 style="font-family:Source Sans Pro Light;"><a href="mypage.php">Paola Ortega</h1></a></li>
+			<li><a href="mypage.php">Home</a></li>
+			<li><a href="hobbies.php">Hobbies</a></li>
+			<li><a href="interests.php">Interests</a></li>
+			<li><a href="trivias.php">Trivia</a></li>
+			<li><a href="add_data.php">Feedback</a></li>
+		</ul>
+	</div>
+</div>
+
+	<div class="prof2-box">
+		<p style="font-family:Courier New" align="center" class="hobby"><font size="10" color="#ab0a0a"><big> HOBBIES </p></font></big>
+		<hr width="100%" color="white">
+		<p style="font-family:Century Gothic" align="center" class="par1"><font size="3"> One of the things i used to spend whenever i have extra time, well, aside from getting enough sleep I really love playing piano at home. Actually, music became my passion. I don't mind spending most of my time practicing or even listening to songs. The classic ones are my favorite and i know that not a day had passed that i hated it. </p></font>
+		<p style="font-family:Century Gothic" align="center"><font size="3"> Listening to music became part of my everyday, it helps me concentrate whenever i'm reviewing for a test or if there is something i need to understand that clearly needed much focus. </p></font>
+		<p style="font-family:Century Gothic" align="center"><font size="3"> Music is best matched with reading, i do enjoy reading while playing classical music in my playlist. I love reading books, but i am often lazy to finish one when i start. </p></font>
+	</div>
+
+<div class="social-icons">
+			<a href="http://www.facebook.com/paolafrancesca.ortega" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+			<a href="http://www.twitter.com/paochipaoch" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+			<a href="https://www.instagram.com/pf.ortega" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+	</div>
+</body>
+
+</html>
