5f4693120c36af537c94a84582d27357b3d390c8
PaolaOrtega <pfortega@outlook.com>
2016-12-09 19:47:01 +0800

    Created trivias_view.php

---
+++ exercise6/codeigniter/application/views/trivias_view.php
@@ -0,0 +1,174 @@
+<!DOCTYPE HTML>
+<html>
+<title>
+	Paola Ortega - Trivias
+</title>
+	
+<head>
+	
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+	
+	<style>
+		html, body, {
+		position:fixed;
+		top:0;
+		bottom:0;
+		left:0;
+		right:0;
+		}
+		
+		html {
+		background: url(trivia.jpg) no-repeat center center fixed; 
+		-webkit-background-size: cover;
+		-moz-background-size: cover;
+		-o-background-size: cover;
+		background-size: cover;
+		}
+		
+		.prof2-box{
+		width: 665px;
+		height: 340px;
+		background: rgba(0,0,0,0);
+		padding: 40px;
+		color: white;
+		margin: 0 auto;
+		margin-top: 60px;
+		text-align: center;
+		}
+
+		.social-icons {
+		font-size: 20px;
+		text-align: Center;
+		position: relative;
+		top: 129px;
+		}
+
+		.social-icons a {
+		color: white;
+		}
+
+		body{
+		font-family: Century Gothic, sans-serif;
+		}
+
+		*{
+		padding: 0px;
+		margin: 0px;
+		}
+
+		#navdiv{
+		opacity: 0.7;
+		filter: (opacity=70);
+		}
+
+		#navdiv ul{
+		width: 100%;
+		height: 80px;
+		background: #312126;
+		line-height: 80px;
+		color: white;
+		margin-right: 15px;
+		}
+
+		#navdiv ul a{
+		text-decoration: none;
+		color: white;
+		padding: 20px;
+		}
+
+		#navdiv ul li{
+		list-style-type: none;
+		display: inline-block;
+		float: right;
+		}
+
+		#navdiv ul li:hover{
+		background: #715a46;
+		transition:all 0.40s;
+		}
+
+		#navdiv h1{
+		width: 300px;
+		float: left;
+		cursor: pointer;
+		margin-left: 15px;
+		}
+
+		.name{
+		color: white;
+		text-align: center;
+		margin-top: 35px;
+		font-size: 70px;
+		}
+
+		.trBorder {
+		height: 130px;
+		width: 600px;
+		margin: 30px;
+		border: 5px solid rgba(255,255,255,.5);
+		position: relative;
+		left: center;
+		top: -150px;
+		}
+ 
+		.dictionary{
+		font-size: 20px;
+		text-align: left;
+		width: 400px;
+		margin-left: 150px;
+		margin-top: -140px;
+		}
+
+		.next {
+		font-size: 100px;
+		margin-left: 320px;
+		margin-top: -190px;
+		padding: 40px;
+		}
+
+		.next a {
+		color: white;
+		}
+
+		.trivia{
+		letter-spacing: 10px;
+		}
+	</style>
+</head>
+<body>
+<div id="Maindiv">
+	<div id="navdiv">
+		
+		<ul>
+			<h1 style="font-family:Source Sans Pro Light;"><a href="mypage.php">Paola Ortega</h1></a></li>
+			<li><a href="mypage.php">Home</a></li>
+			<li><a href="hobbies.php">Hobbies</a></li>
+			<li><a href="interests.php">Interests</a></li>
+			<li><a href="trivias.php">Trivia</a></li>
+			<li><a href="add_data.php">Feedback</a></li>
+		</ul>
+	</div>
+</div>
+
+<div class="prof2-box">
+	<div class="name">
+	<p style= "font-family:Helvetica;" class="trivia"> TRIVIA </p></font>
+	<div class ="trBorder">
+	</div>
+	
+	<p class="dictionary" style="font-family:Helvetica;"> <big><big>trivia</big></big><br> [<strong>triv</strong>-ee-uh]<br>plural noun<br>
+	<small>matters or things that are very<br> unimportant, inconsequential, or<br> nonessential; trifles;<br> trivialities.</small></p></font>
+	
+	<div class="next">
+		<a href="mytrivias.php"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+	</div>
+</div>
+	
+	<div class="social-icons">
+			<a href="http://www.facebook.com/paolafrancesca.ortega" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+			<a href="http://www.twitter.com/paochipaoch" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+			<a href="https://www.instagram.com/pf.ortega" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+	</div>
+</body>
+
+</html>
