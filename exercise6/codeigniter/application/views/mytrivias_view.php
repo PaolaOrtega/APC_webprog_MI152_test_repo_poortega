2361b4f6ef7fd54d6ee0a122da7ca04a507800b3
PaolaOrtega <pfortega@outlook.com>
2016-12-09 19:43:58 +0800

    Created mytrivias_view.php

---
+++ exercise6/codeigniter/application/views/mytrivias_view.php
@@ -0,0 +1,305 @@
+<!DOCTYPE HTML>
+<html>
+<title>
+	Paola Ortega - myTrivias
+</title>
+	
+<head>
+	
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+	
+	<style>
+	
+html, body, {
+    position:fixed;
+    top:0;
+    bottom:0;
+    left:0;
+    right:0;
+}
+
+html {
+  	background: url(book.jpg) no-repeat center center fixed; 
+  	-webkit-background-size: cover;
+  	-moz-background-size: cover;
+  	-o-background-size: cover;
+  	background-size: cover;
+}
+
+.social-icons {
+	font-size: 20px;
+	text-align: Center;
+	position: relative;
+	top: 67px;
+}
+
+.social-icons a {
+	color: white;
+}
+
+body{
+	font-family: Century Gothic, sans-serif;
+}
+
+*{
+	padding: 0px;
+	margin: 0px;
+}
+
+#navdiv{
+	opacity: 0.7;
+	filter: (opacity=70);
+}
+
+#navdiv ul{
+	width: 100%;
+	height: 80px;
+	background: #190e08;
+	line-height: 80px;
+	color: white;
+	margin-right: 15px;
+}
+
+#navdiv ul a{
+	text-decoration: none;
+	color: white;
+	padding: 20px;
+}
+
+#navdiv ul li{
+	list-style-type: none;
+	display: inline-block;
+	float: right;
+}
+
+#navdiv ul li:hover{
+	background: #654f38;
+	transition:all 0.40s;
+}
+
+#navdiv h1{
+	width: 300px;
+	float: left;
+	cursor: pointer;
+	margin-left: 15px;
+}
+
+.prof4-box{
+	width: 50px;
+	height: 400px;
+	background: rgba(0,0,0,0.4);
+	padding: 40px;
+	color: white;
+	margin: 0 auto;
+	margin-top: 40px;
+	margin-left: 10px;
+	text-align: center;
+}
+
+.website-icons {
+	font-size: 40px;
+	margin-top: 20px;
+}
+
+.website-icons a {
+	color: white;
+}
+
+.website{
+	 font-size: 20px;
+	 text-align: left;
+	 width: 400px;
+	 margin-left: -20px;
+	 margin-top: 8px;
+}
+
+.prof5-box{
+	width: 300px;
+	height: 150px;
+	background: rgba(0,0,0,0.4);
+	padding: 10px;
+	color: white;
+	margin: 0;
+	margin-top: 40px;
+	margin-left: 10px;
+	text-align: center;
+}
+
+#demo{
+	margin-top: 25px;
+	margin-bottom: 40px;
+}
+
+.prof6-box{
+	width: 300px;
+	height: 150px;
+	background: rgba(0,0,0,0.4);
+	padding: 10px;
+	color: white;
+	margin: 0;
+	margin-top: 40px;
+	margin-left: 10px;
+	text-align: center;
+}
+
+#demo1{
+	margin-top: 25px;
+	margin-bottom: 20px;
+}
+
+.prof7-box{
+	width: 300px;
+	height: 150px;
+	background: rgba(0,0,0,0.4);
+	padding: 10px;
+	color: white;
+	margin: 0;
+	margin-top: -380px;
+	margin-left: 690px;
+	text-align: center;
+}
+
+#demo2{
+	margin-top: 25px;
+	margin-bottom: 40px;
+}
+
+.prof8-box{
+	width: 300px;
+	height: 150px;
+	background: rgba(0,0,0,0.4);
+	padding: 10px;
+	color: white;
+	margin: 0;
+	margin-top: -170px;
+	margin-left: 350px;
+	text-align: center;
+}
+
+#demo3{
+	margin-top: 25px;
+	margin-bottom: 40px;
+}
+
+.prof9-box{
+	width: 300px;
+	height: 150px;
+	background: rgba(0,0,0,0.4);
+	padding: 10px;
+	color: white;
+	margin: 0;
+	margin-top: 40px;
+	margin-left: 350px;
+	text-align: center;
+}
+
+#demo4{
+	margin-top: 25px;
+	margin-bottom: 40px;
+}
+
+.prof10-box{
+	width: 300px;
+	height: 150px;
+	background: rgba(0,0,0,0.4);
+	padding: 10px;
+	color: white;
+	margin: 0;
+	margin-top: -170px;
+	margin-left: 690px;
+	text-align: center;
+}
+
+#demo5{
+	margin-top: 25px;
+	margin-bottom: 40px;
+}
+
+.cover{
+	width: 1030px;
+	height: 450px;
+	background: rgba(0,0,0,0);
+	padding: 10px;
+	color: white;
+	margin: 0;
+	margin-top: -500px;
+	margin-left: 15%;
+	text-align: center;
+}
+	</style>
+</head>
+<body>
+<div id="Maindiv">
+	<div id="navdiv">
+		
+		<ul>
+			<h1 style="font-family:Source Sans Pro Light;"><a href="mypage.php">Paola Ortega</h1></a></li>
+			<li><a href="mypage.php">Home</a></li>
+			<li><a href="hobbies.php">Hobbies</a></li>
+			<li><a href="interests.php">Interests</a></li>
+			<li><a href="trivias.php">Trivia</a></li>
+			<li><a href="add_data.php">Feedback</a></li>
+		</ul>
+	</div>
+</div>
+
+<div class="prof4-box">
+		<p style="font-family:Britannic Bold" class="website"><font size="5">websites </p></font>
+		
+		<div class="website-icons">
+			<a href="https://www.spotify.com/ph" target="_blank"><i class="fa fa-spotify" aria-hidden="true"></i></a>
+			<a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+			<a href="https://www.twitter.com/" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+			<a href="https://www.pinterest.com/" target="_blank"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+			<a href="https://www.wordpress.com/" target="_blank"><i class="fa fa-wordpress" aria-hidden="true"></i></a>
+			<a href="https://www.deviantart.com/" target="_blank"><i class="fa fa-deviantart" aria-hidden="true"></i></a>
+			<a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+		</div>
+</div>
+
+<div class="social-icons">
+			<a href="http://www.facebook.com/paolafrancesca.ortega" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+			<a href="http://www.twitter.com/paochipaoch" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+			<a href="https://www.instagram.com/pf.ortega" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+	</div>
+	
+<div class="cover">
+<div class="prof5-box">
+	<p id="demo">One of my favorite movie<br>starring Rachel Mcdams</p>
+	
+	<button type="button" onclick="document.getElementById('demo').innerHTML = 'About Time'">ANSWER</button>
+</div>
+	
+<div class="prof6-box">
+	<p id="demo1">The book that I would love to<br>bring if ever I get stuck <br>on an island</p>
+	
+	<button type="button" onclick="document.getElementById('demo1').innerHTML = 'The Great Gatsby'">ANSWER</button>
+</div>
+	
+<div class="prof7-box">
+	<p id="demo2">My favorite instrument<br>to play</p>
+	
+	<button type="button" onclick="document.getElementById('demo2').innerHTML = 'Piano'">ANSWER</button>
+</div>
+	
+<div class="prof8-box">
+	<p id="demo3">My past time aside from<br>sleeping</p>
+	
+	<button type="button" onclick="document.getElementById('demo3').innerHTML = 'Watching movies'">ANSWER</button>
+</div>
+
+<div class="prof9-box">
+	<p id="demo4">Favorite band since<br>highschool</p>
+	
+	<button type="button" onclick="document.getElementById('demo4').innerHTML = 'Maroon 5'">ANSWER</button>
+</div>
+
+<div class="prof10-box">
+	<p id="demo5">I have allergy on these and <br>it makes me sneeze</p>
+	
+	<button type="button" onclick="document.getElementById('demo5').innerHTML = 'Dusts'">ANSWER</button>
+</div>
+</div>
+</body>
+
+</html>
