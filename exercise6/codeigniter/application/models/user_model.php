9357dce45ed5fc6dd1a379f580bf12a65d8119a8
PaolaOrtega <pfortega@outlook.com>
2016-12-09 17:40:22 +0800

    Added function set_users

--- exercise6/codeigniter/application/models/user_model.php
+++ exercise6/codeigniter/application/models/user_model.php
@@ -30,6 +30,33 @@ class User_model extends CI_Model {
         return $query->row_array();
     }
     
+     public function set_users($id = 0)
+    {
+        $this->load->helper('url');
+ 
+        $slug = url_title($this->input->post('name'), 'dash', TRUE);
+ 
+        $data = array(
+        'name' => $this->input->post('name'),
+        'email' => $this->input->post('email'),
+        'gender' => $this->input->post('gender'),
+        'nickname' => $this->input->post('nickname'),
+        'hadd' => $this->input->post('hadd'),
+        'cpnum' => $this->input->post('cpnum'),
+        'slug' => $slug,
+        'comment' => $this->input->post('comment')
+           
+        );
+        
+        if ($id == 0) {
+            return $this->db->insert('users', $data);
+        } else {
+            $this->db->where('id', $id);
+            return $this->db->update('users', $data);
+        }
+    }
+    
+
     public function delete_users($id)
     {
         $this->db->where('id', $id);
