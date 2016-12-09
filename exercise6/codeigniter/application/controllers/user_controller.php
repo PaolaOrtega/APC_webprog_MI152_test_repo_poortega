4f76ea52de821d29d64d99923080722c124152b5
PaolaOrtega <pfortega@outlook.com>
2016-12-09 17:24:53 +0800

    Added public function for pages in website

--- exercise6/codeigniter/application/controllers/user_controller.php
+++ exercise6/codeigniter/application/controllers/user_controller.php
@@ -114,6 +114,72 @@ class User_Controller extends CI_Controller {
         redirect( base_url() . 'index.php/user_controller');        
     }
 
+    public function mypage()
+    {   
+        //load view and pass the data
+        $this->load->helper('url');
+        $this->load->view('mypage_view');
+    }
+
+    public function hobbies()
+    {
+     
+        //load view and pass the data
+        $this->load->helper('url');
+        $this->load->view('hobbies_view');
+    }
+
+     public function information()
+    {
+     
+        //load view and pass the data
+        $this->load->helper('url');
+        $this->load->view('information_view');
+    }
+
+     public function interests()
+    {
+     
+        //load view and pass the data
+        $this->load->helper('url');
+        $this->load->view('interests_view');
+    }
+
+     public function myhobbies()
+    {
+        //load view and pass the data
+        $this->load->helper('url');
+        $this->load->view('myhobbies_view');
+    }
+
+        public function myinterests()
+    {
+        //load view and pass the data
+        $this->load->helper('url');
+        $this->load->view('myinterests_view');
+    }
+    
+        public function mytrivias()
+    {
+        //load view and pass the data
+        $this->load->helper('url');
+        $this->load->view('mytrivias_view');
+    }
+
+        public function trivias()
+    {
+        //load view and pass the data
+        $this->load->helper('url');
+        $this->load->view('trivias_view');
+    }
+
+        public function add_data()
+    {  
+        //load view and pass the data
+        $this->load->helper('url');
+        $this->load->view('add_data_view');
+    }
+}
 
 
 
