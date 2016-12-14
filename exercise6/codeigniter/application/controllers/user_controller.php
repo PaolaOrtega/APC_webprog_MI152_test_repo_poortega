9bd170df5df1223e987903e93a2beeee2d0b7230
PaolaOrtega <pfortega@outlook.com>
2016-12-14 15:59:03 +0800

    removed url

--- exercise6/codeigniter/application/controllers/user_controller.php
+++ exercise6/codeigniter/application/controllers/user_controller.php
@@ -117,7 +117,7 @@ class User_Controller extends CI_Controller {
     public function mypage()
     {   
         //load view and pass the data
-        $this->load->helper('url');
+       
         $this->load->view('mypage_view');
     }
 
@@ -125,7 +125,7 @@ class User_Controller extends CI_Controller {
     {
      
         //load view and pass the data
-        $this->load->helper('url');
+   
         $this->load->view('hobbies_view');
     }
 
@@ -133,7 +133,7 @@ class User_Controller extends CI_Controller {
     {
      
         //load view and pass the data
-        $this->load->helper('url');
+   
         $this->load->view('information_view');
     }
 
@@ -141,42 +141,42 @@ class User_Controller extends CI_Controller {
     {
      
         //load view and pass the data
-        $this->load->helper('url');
+ 
         $this->load->view('interests_view');
     }
 
      public function myhobbies()
     {
         //load view and pass the data
-        $this->load->helper('url');
+   
         $this->load->view('myhobbies_view');
     }
 
         public function myinterests()
     {
         //load view and pass the data
-        $this->load->helper('url');
+    
         $this->load->view('myinterests_view');
     }
     
         public function mytrivias()
     {
         //load view and pass the data
-        $this->load->helper('url');
+     
         $this->load->view('mytrivias_view');
     }
 
         public function trivias()
     {
         //load view and pass the data
-        $this->load->helper('url');
+  
         $this->load->view('trivias_view');
     }
 
         public function add_data()
     {  
         //load view and pass the data
-        $this->load->helper('url');
+    
         $this->load->view('add_data_view');
     }
 }
